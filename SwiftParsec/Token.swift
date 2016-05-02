039105a57e52c8c3736a49f2761ca68e9879e12c
David Dufresne <davedufresne@gmail.com>
2016-05-02 17:47:08 -0400

    Move the `LanguageDefinition` struct in Language.swift
    Even if the `LanguageDefinition` struct is related to the tokenizer in
    belongs in its own file.

--- SwiftParsec/Token.swift
+++ SwiftParsec/Token.swift
@@ -9,47 +9,6 @@
 
 import Foundation
 
-/// The `LanguageDefinition` structure contains all parameterizable features of the token parser. There is some default definitions provided by SwiftParsec.
-public struct LanguageDefinition<UserState> {
-    
-    /// Describe the start of a block comment. Use the empty string if the language doesn't support block comments. For example "/*".
-    public var commentStart: String
-    
-    /// Describe the end of a block comment. Use the empty string if the language doesn't support block comments. For example "*/".
-    public var commentEnd: String
-    
-    /// Describe the start of a line comment. Use the empty string if the language doesn't support line comments. For example "//".
-    public var commentLine: String
-    
-    /// Set to `true` if the language supports nested block comments.
-    public var allowNestedComments: Bool
-    
-    /// This parser should accept any start characters of identifiers. For example `letter <|> character("_")`.
-    public var identifierStart: GenericParser<String, UserState, Character>
-    
-    /// This parser should accept any legal tail characters of identifiers. For example `alphaNum <|> character("_")`. The function receives the character parsed by `identifierStart` as parameter, allowing to handle special cases (i.e. implicit parameters in swift start with a '$' that must be followed by decimal digits only).
-    public var identifierLetter: Character -> GenericParser<String, UserState, Character>
-    
-    /// This parser should accept any start characters of operators. For example `oneOf(":!#$%&*+./<=>?@\\^|-~")`
-    public var operatorStart: GenericParser<String, UserState, Character>
-    
-    /// This parser should accept any legal tail characters of operators. Note that this parser should even be defined if the language doesn't support user-defined operators, or otherwise the `reservedOperators` parser won't work correctly.
-    public var operatorLetter: GenericParser<String, UserState, Character>
-    
-    /// The set of reserved identifiers.
-    public var reservedNames: Set<String>
-    
-    /// The set of reserved operators.
-    public var reservedOperators: Set<String>
-    
-    /// This optional parser should accept escaped characters. This parser will also replace the string gap and zero-width escape sequence parsers. The default escape sequences have the following form: '\97' '\x61', '\o141', '\^@', '\n', \NUL.
-    public var characterEscape: GenericParser<String, UserState, Character>?
-    
-    /// Set to `true` if the language is case sensitive.
-    public var isCaseSensitive: Bool
-    
-}
-
 /// Types implementing this protocol hold lexical parsers.
 public protocol TokenParserType {
     
