ba1a32f8ba5dafdcd8999ba928e8c5bb75a7f156
David Dufresne <davedufresne@gmail.com>
2016-05-25 09:40:08 -0400

    Reorganize source code in the file system for Swift Package Manager
    Reorganize source code to be able to use the Swift Package Manager 0.1.

---
+++ Package.swift
@@ -0,0 +1,13 @@
+//
+//  Package.swift
+//  SwiftParsec
+//
+//  Created by David Dufresne on 2016-05-10.
+//  Copyright © 2016 David Dufresne. All rights reserved.
+//
+
+import PackageDescription
+
+let package = Package(
+    name: "SwiftParsec"
+)
