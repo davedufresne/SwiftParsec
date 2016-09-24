7aa22ca6667f733f7b6cc44ce5668911f585cace
David Dufresne <davedufresne@gmail.com>
2016-09-24 15:54:59 -0400

    Split `Character` extension in two logical files
    Move methods related to testing the membership of a character in their
    own file. And move the methods related to converting a character in a
    different format in another file.

---
+++ Sources/SwiftParsec/CharacterConversion.swift
@@ -0,0 +1,37 @@
+//
+//  CharacterConversion.swift
+//  SwiftParsec
+//
+//  Created by David Dufresne on 2016-09-24.
+//  Copyright © 2016 David Dufresne. All rights reserved.
+//
+
+import Foundation
+
+extension Character {
+    
+    /// The first `UnicodeScalar` of `self`.
+    var unicodeScalar: UnicodeScalar {
+        
+        let unicodes = String(self).unicodeScalars
+        return unicodes[unicodes.startIndex]
+        
+    }
+    
+    /// Lowercase `self`.
+    var lowercase: Character {
+        
+        let str = String(self).lowercased()
+        return str[str.startIndex]
+        
+    }
+    
+    /// Uppercase `self`.
+    var uppercase: Character {
+        
+        let str = String(self).uppercased()
+        return str[str.startIndex]
+        
+    }
+    
+}
--- Sources/SwiftParsec/CharacterMembership.swift
+++ Sources/SwiftParsec/CharacterMembership.swift
@@ -1,5 +1,5 @@
 //
-//  CharacterExtension.swift
+//  CharacterMembership.swift
 //  SwiftParsec
 //
 //  Created by David Dufresne on 2015-09-19.
@@ -16,14 +16,6 @@ private let symbolSet = CharacterSet.symbols
 private let digitSet = CharacterSet.decimalDigits
 
 extension Character {
-    
-    /// The first `UnicodeScalar` of `self`.
-    var unicodeScalar: UnicodeScalar {
-        
-        let unicodes = String(self).unicodeScalars
-        return unicodes[unicodes.startIndex]
-        
-    }
     
     /// True for any space character, and the control characters \t, \n, \r, \f,
     /// \v.
@@ -144,22 +136,6 @@ extension Character {
     var isOctalDigit: Bool {
         
         return "01234567".characters.contains(self)
-        
-    }
-    
-    /// Lowercase `self`.
-    var lowercase: Character {
-        
-        let str = String(self).lowercased()
-        return str[str.startIndex]
-        
-    }
-    
-    /// Uppercase `self`.
-    var uppercase: Character {
-        
-        let str = String(self).uppercased()
-        return str[str.startIndex]
         
     }
     
