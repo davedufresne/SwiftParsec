a15b1d5cb5fd034bf9afc53e806903970ff5c330
Gerry Power <gerry@thepowerhouse.com>
2019-09-12 07:45:18 -0400

    Change travis config to support Swift 5

--- scripts/install-swift.sh
+++ scripts/install-swift.sh
@@ -2,7 +2,7 @@
 
 set -ev
 
-SWIFT_VERSION="3.0.2"
+SWIFT_VERSION="5.0.3"
 SWIFT_URL="https://swift.org/builds/swift-${SWIFT_VERSION}-release/ubuntu1404/swift-${SWIFT_VERSION}-RELEASE/swift-${SWIFT_VERSION}-RELEASE-ubuntu14.04.tar.gz"
 
 if [ "${TRAVIS_OS_NAME}" = "linux" ]; then
